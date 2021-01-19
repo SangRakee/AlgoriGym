c3f1a4d6984cd493289d09e5ca669420932ba1b1
SangRakKun <leesangrak0307@gmail.com>
2021-01-20 08:48:10 +0900

    20210120 문제추가

---
+++ Programmers/행렬의 곱셈.py
@@ -0,0 +1,16 @@
+arr1=[[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+arr2=[[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+
+def solution(arr1, arr2):
+    #answer = [[]]
+    answer=[[0]*len(arr2[0]) for i in range(len(arr1))]
+
+
+    for i in range(len(arr1)):
+        for j in range(len(arr2[0])):
+            for k in range(len(arr1[0])):
+                answer[i][j] += (arr1[i][k] * arr2[k][j])
+
+    return answer
+
+print(solution(arr1,arr2))
