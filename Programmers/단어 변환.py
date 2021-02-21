b0d7f884ddfd88f5d97921104efb6b37e039f3c6
SangRakKun <leesangrak0307@gmail.com>
2021-02-21 16:04:05 +0900

    단어변환 수정

--- Programmers/단어 변환.py
+++ Programmers/단어 변환.py
@@ -7,6 +7,7 @@ from collections import deque
 def bfs(begin, target, words,visit):
     queue = deque([begin])
     index = words.index(target)
+    
     while queue:
         text = queue.popleft()
         if text == target:
@@ -14,8 +15,8 @@ def bfs(begin, target, words,visit):
 
         for i in range(1, len(words)):
             sum_of_word = 0
-            if not visit[i]:
-                for a, b in zip(text, words[i]):
+            for a, b in zip(text, words[i]):
+                if not visit[i]:
                     if a == b:
                         sum_of_word += 1
 
@@ -30,10 +31,6 @@ def bfs(begin, target, words,visit):
 def solution(begin, target, words):
     answer = 0
     visit = [0] * (len(words)+1)
-
-    # target이 words안에 존재하지 않으면
-    if target not in words:
-        return 0
 
     words.insert(0,begin)
     answer = bfs(begin, target, words,visit)
