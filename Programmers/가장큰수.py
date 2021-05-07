7abc6508c1d22674b6c3b876103fd3c87a7c5d8b
SangRakee <61980851+SangRakee@users.noreply.github.com>
2021-05-08 00:58:53 +0900

    Update 가장큰수.py
    20210508 문제추가

--- Programmers/가장큰수.py
+++ Programmers/가장큰수.py
@@ -1,3 +1,29 @@
+# from functools import cmp_to_key
+
+
+# def comp(x, y):
+#     if int(x + y) < int(y + x):
+#         return True
+#     elif int(x + y) == int(y + x):
+#         return False
+#     else:
+#         return -1
+
+
+# def solution(numbers):
+#     answer = ""
+#     num = []
+#     for i in range(len(numbers)):
+#         num.append(str(numbers[i]))
+
+#     sorted_num = sorted(num, key=cmp_to_key(comp))
+
+#     for i in range(len(sorted_num)):
+#         answer += sorted_num[i]
+
+#     answer = str(int(answer))
+#     return answer
+
 def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key = lambda x: x*3, reverse=True)
