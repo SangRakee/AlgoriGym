de0d3cdefb219386aab13b0b26ca7ca057df6bf6
SangRakKun <leesangrak0307@gmail.com>
2021-05-07 13:24:28 +0900

    20210507 문제추가

---
+++ Programmers/주식가격.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    for (int i = 0; i < prices.size(); i++) {
+        int price = 0;
+        for (int j = i + 1; j < prices.size(); j++) {
+            if (prices[j] >= prices[i]) price++;
+            else {
+                price++;
+                break;
+            }
+        }
+        answer.push_back(price);
+    }
+    return answer;
+}
