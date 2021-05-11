c0a4fa31d73f865a89a3c89119f760703b3f7f60
SangRakKun <leesangrak0307@gmail.com>
2021-05-11 20:34:19 +0900

    20210511 문제추가

---
+++ Programmers/입국심사.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0, minTime = 1, maxTime, avgTime, human = 0;
+    maxTime = *max_element(times.begin(), times.end()) * (long long)n;
+    while (minTime <= maxTime) {
+        avgTime = ((maxTime + minTime) / 2);
+        for (auto t : times)        human += avgTime / t;
+        if (n <= human) {
+            answer = avgTime;
+            maxTime = avgTime - 1;
+        }
+        else    minTime = avgTime + 1;
+        human = 0;
+    }
+    return answer;
+}
