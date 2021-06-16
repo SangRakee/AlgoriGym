d8afe2a261e55649329f2ec18392fa7403594f81
SangRakKun <leesangrak0307@gmail.com>
2021-06-16 21:29:50 +0900

    20210616 코드변환

--- Programmers/위장.py
+++ Programmers/위장.py
@@ -1,4 +1,18 @@
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key = lambda x: x*3, reverse=True)
-    return str(int(''.join(numbers)))
+#약수 개수 구하는 방식 사용
+
+def solution(clothes):
+    answer = {}
+    for i in clothes:
+        if i[1] in answer: answer[i[1]] += 1
+        else: answer[i[1]] = 1
+
+    cnt = 1
+    for i in answer.values():
+        cnt *= (i+1)
+    return cnt - 1
+
+
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key = lambda x: x*3, reverse=True)
+#     return str(int(''.join(numbers)))
