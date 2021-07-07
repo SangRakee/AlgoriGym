0bdbe57b42c4d19d325ea68a076928ae9ed6562c
SangRakKun <leesangrak0307@gmail.com>
2021-07-07 09:57:29 +0900

    20210707 문제추가

--- Programmers/완주하지 못한 선수.py
+++ Programmers/완주하지 못한 선수.py
@@ -1,7 +1,11 @@
+# 첫번째 방법
 # 처음에는 그냔 for안에 if문으로 in을 사용하여 전부 확인하는 방식으로 했지만 시간초과가 발생하였음
 # 각 리스트를 정렬하여 인덱스가 다른 곳을 return하는 방식
 
-def solution(participant, completion):
+# 두번째 방법
+# 해시사용
+
+def solution1(participant, completion):
     participant.sort()
     completion.sort()
 
@@ -11,3 +15,22 @@ def solution(participant, completion):
             return p
 
     return participant.pop()
+
+
+def solution2(participant, completion):
+    answer = ''
+
+    dict = {}
+    for i in participant:
+        dict[i] = 0
+    for i in participant:
+        dict[i] += 1
+    for i in completion:
+        dict[i] -= 1
+
+    print(dict)
+    for i,j in zip(dict.keys(),dict.values()):
+        if j==1:
+            answer=i
+
+    return answer
