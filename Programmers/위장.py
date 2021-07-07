f772ece0fb065ad24a6fc049e0d63ba49865caa3
SangRakKun <leesangrak0307@gmail.com>
2021-07-07 10:07:41 +0900

    20210707 코드 추가

--- Programmers/위장.py
+++ Programmers/위장.py
@@ -1,18 +1,22 @@
 #약수 개수 구하는 방식 사용
 
-def solution(clothes):
-    answer = {}
+def solution1(clothes):
+    answer = 1
+    dict={}
+    
     for i in clothes:
-        if i[1] in answer: answer[i[1]] += 1
-        else: answer[i[1]] = 1
+        if i[1] in dict:
+            dict[i[1]]+=1
+        else:
+            dict[i[1]]=1
+    
+    for i in dict.values():
+        answer*=(i+1)
+    
+    return answer-1
 
-    cnt = 1
-    for i in answer.values():
-        cnt *= (i+1)
-    return cnt - 1
 
-
-# def solution(numbers):
-#     numbers = list(map(str, numbers))
-#     numbers.sort(key = lambda x: x*3, reverse=True)
-#     return str(int(''.join(numbers)))
+def solution2(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
